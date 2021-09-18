abf762026fa09cec37cfd821ad5e1615a26c6f49
ask@ceconline.edu <ask@ceconline.edu>
2021-09-18 10:04:35 +0530

    delete image moved to aassets/image folder, rails image_tag used

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,8 +17,6 @@ class TodosController < ApplicationController
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    #response_text = "Hey, your new todo is created with the id #{new_todo.id}"
-    #render plain: response_text
     redirect_to todos_path
   end
 
@@ -28,7 +26,6 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    #render plain: "Updated todo completed status to #{completed}"
     redirect_to todos_path
   end
 end

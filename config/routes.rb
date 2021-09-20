1938330a7121d91bd75f06f5956680c6ee09d4da
ask@ceconline.edu <ask@ceconline.edu>
2021-09-20 22:33:40 +0530

    sign-out implemented, home->signup link is not working

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/" => "home#index"
+  resources :todos
 
-  post "/users/login" => "users#login"
+  resources :users
+
+  #post "/users/login" => "users#login"
 
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
-
-  resources :todos, :users
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end

5872a57a98cdb9473aadcdf2647e5e29165d4fc0
ask@ceconline.edu <ask@ceconline.edu>
2021-09-21 15:56:16 +0530

    todo validation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -19,11 +19,18 @@ class TodosController < ApplicationController
 
   def create
     todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
+    #due_date = DateTime.parse(params[:due_date])
+    due_date = params[:due_date]
+
     user_id = current_user.id
-    new_todo = Todo.create!(todo_text: todo_text,
-                            due_date: due_date, completed: false, user_id: user_id)
-    redirect_to todos_path
+    new_todo = Todo.new(todo_text: todo_text,
+                        due_date: due_date, completed: false, user_id: user_id)
+    if new_todo.save
+      redirect_to todos_path
+    else
+      flash[:error] = new_todo.errors.full_messages.join(", ")
+      redirect_to todos_path
+    end
   end
 
   def update
